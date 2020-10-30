fd5e95dec2f3bff448f7d9982d75444f24199c07
brandolkuete <brandolkuete95@gmail.com>
2020-10-30 22:48:42 +0100

    Updates

---
+++ precache-manifest.87848e859817a57c1e6be44c5834afa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c06594ca9d42d6422818bdcf9b3805dc",
+    "url": "/ToDoList-reactJS/index.html"
+  },
+  {
+    "revision": "c3b5b732b75522a8319d",
+    "url": "/ToDoList-reactJS/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c3b5b732b75522a8319d",
+    "url": "/ToDoList-reactJS/static/js/2.bc06dcd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoList-reactJS/static/js/2.bc06dcd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49fa16ae155f369e8fe2",
+    "url": "/ToDoList-reactJS/static/js/main.895a0149.chunk.js"
+  },
+  {
+    "revision": "aa37c9707fe34ec29678",
+    "url": "/ToDoList-reactJS/static/js/runtime-main.9d62f9d4.js"
+  }
+]);
